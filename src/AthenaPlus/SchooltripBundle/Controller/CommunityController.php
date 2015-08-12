521d48dec27e1b2a033d3deef899656dbdfd1a2a
Pieter De Praetere <pieter@packed.be>
2015-08-12 15:02:38 +0200

    Some refactoring.

--- src/AthenaPlus/SchooltripBundle/Controller/CommunityController.php
+++ src/AthenaPlus/SchooltripBundle/Controller/CommunityController.php
@@ -19,59 +19,6 @@ use AthenaPlus\SchooltripBundle\Form\PostType;
 
 class CommunityController extends Controller
 {
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        // new form
-        $post = new Post();
-        $postForm   = $this->createCreateForm($post);
-
-        if ( $request->isMethod( 'POST' ) ) {
-            $postForm->handleRequest( $request );
-            if ( $postForm->isValid( ) ) {
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($post);
-                $em->flush();
-                // todo: put in service
-
-                $notif = new Notification();
-                $notif->setType("Post");
-                $notif->setUser($this->getUser());
-                $notif->setContentId($post->getId());
-
-
-                $url = $this->container->get('router')->generate(
-                    'community_index'
-                );
-
-                $message = "<span class='fat'>{$this->getUser()}</span> placed a <a href='$url'>message</a> in the <a href='$url'>notification section.</a> ";
-
-                $notif->setMessage($message);
-
-                $em->persist($notif);
-                $em->flush();
-
-                return $this->createJsonResponse(true);
-
-            } else{
-                $response['success'] = false;
-                return new JsonResponse($response);
-            }
-
-
-        }
-
-        $posts = $em->getRepository('SchooltripBundle:Post')->findLast();
-
-
-        return $this->render('SchooltripBundle:Community:index.html.twig', array(
-            'posts' => $posts,
-            'post' => $post,
-            'post_form'   => $postForm->createView(),
-        ));
-
-    }
 
     public function loadAction(Request $request){
         $offset = $request->get('offset');
@@ -119,6 +66,54 @@ class CommunityController extends Controller
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse|Response
+     */
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        /* Empty form */
+        $post = new Post();
+        $postForm = $this->createCreateForm($post);
+        /* If the request is POST, a new item on the board was created */
+        if ($request->isMethod('POST')) {
+            $postForm->handleRequest($request);
+            if ($postForm->isValid()) {
+                $em->persist($post);
+                $em->flush();
+                $notif = new Notification();
+                $notif->setType("Post");
+                $notif->setUser($user);
+                $notif->setContentId($post->getId());
+                $url = $this->container->get('router')->generate(
+                    'community_index'
+                );
+                $message = "<span class='fat'>{$this->getUser()}</span> placed a <a href='$url'>message</a> in the <a href='$url'>notification section.</a> ";
+                $notif->setMessage($message);
+                $em->persist($notif);
+                $em->flush();
+                return $this->createJsonResponse(true);
+            } else {
+                $response['success'] = false;
+                return new JsonResponse($response);
+            }
+        }
+        $group = $user->getGroup();
+        if ($group) {
+            $posts = $em->getRepository('SchooltripBundle:Post')->findLast();
+        } else {
+            /* Admins are not in a group, so show all posts */
+            $posts = $em->getRepository('SchooltripBundle:Post')->findLast();
+        }
+        return $this->render('SchooltripBundle:Community:index.html.twig', array(
+            'posts' => $posts,
+            'post' => $post,
+            'post_form'   => $postForm->createView(),
+        ));
+
     }
 
 
--- src/AthenaPlus/SchooltripBundle/Entity/PostRepository.php
+++ src/AthenaPlus/SchooltripBundle/Entity/PostRepository.php
@@ -12,25 +12,42 @@ use Doctrine\ORM\Query;
  */
 class PostRepository extends EntityRepository
 {
-    public function findLast($number = 10, $offset = 0)
-    {
+
+    /**
+     * Find the last $number posts from $offset
+     * @param int $number
+     * @param int $offset
+     * @return array
+     */
+    public function findLast ($number = 10, $offset = 0) {
         $result = $this->getEntityManager()
-            ->createQuery(
-                'SELECT p FROM SchooltripBundle:Post p ORDER BY p.created DESC'
-            )
+            ->createQuery('SELECT p FROM SchooltripBundle:Post p ORDER BY p.created DESC')
             ->setMaxResults($number)
             ->setFirstResult($offset)
             ->getResult();
-        $posts = array();
-        foreach ($result as $post){
-            $newPost = array();
-            $newPost['id'] = $post->getId();
-            $newPost['created'] = $post->getCreated();
-            $newPost['created_by'] = $post->getCreatedBy() ? $post->getCreatedBy()->getUsername() : "-";
-            $newPost['text'] = $post->getText();
-            $posts[] = $newPost;
-        }
+        return $this->convert_result_to_post_list($result);
+    }
 
+    /**
+     * Convert the result of a query in the Post table to a list acceptable to the display functions in
+     * CommunityController. Accepts a query-result and returns an array in the reversed order.
+     * @param $result
+     * @return array
+     */
+    protected function convert_result_to_post_list($result) {
+        $posts = array ();
+        foreach ($result as $post) {
+            $new_post = array();
+            $new_post['id'] = $post->getId();
+            $new_post['created'] = $post->getCreated();
+            if ($post->getCreatedBy()) {
+                $new_post['created_by'] = $post->getCreatedBy()->getUsername();
+            } else {
+                $new_post['created_by'] = '-';
+            }
+            $new_post['text'] = $post->getText();
+            array_push($posts, $new_post);
+        }
         return array_reverse($posts);
     }
 }
