b3d4080ef30e280e6a99ad4b0d32465b7fb5bd1e
Pieter De Praetere <pieter@packed.be>
2015-08-12 09:54:10 +0200

    Refactoring and bugfix that caused images to be displayed very small in the image edit interface.

--- web/js/base.js
+++ web/js/base.js
@@ -175,50 +175,6 @@ function updateProfile(){
 }
 
 
-function getGroups(){
-    var groups = [];
-    $('.trip-group').each(function(){
-        var group = {};
-        group.id   = $(this).attr('data-id');
-        group.name = $(this).find('.groupDescription').html();
-        groups.push(group);
-    });
-    return groups;
-}
-
-$('.modaleditbutton').on('click', function(){
-
-});
-
-function loadBaseGroups(){
-    $.ajax({
-        'type' : 'POST',
-        'url'  : Routing.generate('load-groups'),
-        'success': function(groups){
-            var html = drawGroups(groups);
-            $('.group-list').html(html);
-        },
-        'error': function(){
-            alert("An error has occurred. Please try again...");
-        }
-    });
-}
-
-function deleteGroup(id){
-    $.ajax({
-        'type' : 'POST',
-        'url'  : Routing.generate('delete-group', {id: id}),
-        'success': function(groups){
-            alert('Group deleted');
-            var html = drawGroups(groups);
-            $('.group-list').html(html);
-        },
-        'error': function(){
-            alert("An error has occurred. Please try again...");
-        }
-    });
-}
-
 
 $('.settingNotif').on('click', function(){
     $(this).toggleClass('activated');
